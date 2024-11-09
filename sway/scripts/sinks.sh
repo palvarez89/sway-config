ea2d8c77aff46a7e262663352d0ec4f69aa52032
Pedro Alvarez <pedro@alvarezpiedehierro.com>
2024-11-09 23:09:57 +0000

    WIP script change to new sink

---
+++ sway/scripts/sinks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+default_sink_name=$(pw-metadata 0 'default.audio.sink' | grep 'value' | sed "s/.* value:'//;s/' type:.*$//;" | jq .name)
+default_sink_id=$(pw-dump Node Device | jq '.[].info.props|select(."node.name" == '" $default_sink_name "')|."object.id"')
+other_sink_id=$(pw-dump Node Device | jq '.[].info.props|select(."api.alsa.pcm.stream" == "playback")|select(."object.id" != '" $default_sink_id "')| (."object.id"|tostring) + " " + ."node.nick"')
+
+echo $default_sink_id
+echo $other_sink_id | tr '"' '\n' | grep -v '^\s*$' | sort -n
+selection=$(echo $other_sink_id | tr '"' '\n' | grep -v '^\s*$' | sort -n| rofi -dmenu -p="Select Audio Output")
+echo $selection
+
